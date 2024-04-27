b96f85c914ef37cbd08f1d39b894bff1314a3ac3
chansoo <chansoo38@gmail.com>
2024-04-27 15:55:24 +0900

    feat : 프로젝트 생성 #1

---
+++ seekgu/src/main/java/com/seekgu/SeekguApplication.java
@@ -0,0 +1,13 @@
+package com.seekgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeekguApplication.class, args);
+    }
+
+}
---
+++ seekgu/src/test/java/com/seekgu/SeekguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seekgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
