19aea0b3a42a42bbb86cb2ec1afe81c908202f53
chansoo <chansoo38@gmail.com>
2024-04-29 13:19:00 +0900

    feat : SeekguBoardRespository Mapper 구현

---
+++ seekgu/src/main/java/com/seekgu/seekguboard/repository/SeekguBoardRepository.java
@@ -0,0 +1,17 @@
+package com.seekgu.seekguboard.repository;
+
+import com.seekgu.seekguboard.domain.SeekguBoard;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface SeekguBoardRepository {
+    SeekguBoard findSeekguBoardByIdx(Long seekguIdx);
+    Boolean save(SeekguBoard seekguBoard);
+    List<SeekguBoard> findRecruitingSeekguBoard();
+    List<SeekguBoard> findDoneSeekguBoard();
+    List<SeekguBoard> findMySeekguBoard(Long memberIdx);
+    SeekguBoard getSeekguBoardForUpdate(Long seekguIdx);
+    Boolean participate(Long seekguIdx);
+}
