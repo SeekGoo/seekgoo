0f74e3f31bb45bb01c1915405d4fab3f250b0a34
chansoo <chansoo38@gmail.com>
2024-04-29 14:13:32 +0900

    feat : SeekguBoardService 구현

---
+++ seekgu/src/main/java/com/seekgu/seekguboard/service/SeekguBoardService.java
@@ -0,0 +1,40 @@
+package com.seekgu.seekguboard.service;
+
+import com.seekgu.seekguboard.domain.SeekguBoard;
+import com.seekgu.seekguboard.repository.SeekguBoardRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SeekguBoardService {
+
+    private final SeekguBoardRepository seekguBoardRepository;
+
+    @Transactional
+    public Boolean createSeekguBoard(SeekguBoard seekguBoard) {
+        try {
+            return seekguBoardRepository.save(seekguBoard);
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    public List<SeekguBoard> mySeekguBoards(Long memberIdx) {
+        return seekguBoardRepository.findMySeekguBoard(memberIdx);
+    }
+
+    public List<SeekguBoard> recruitingSeekguBoards() {
+        return seekguBoardRepository.findRecruitingSeekguBoard();
+    }
+
+    public List<SeekguBoard> doneSeekguBoards() {
+        return seekguBoardRepository.findDoneSeekguBoard();
+    }
+
+    public SeekguBoard findSeekguBoardById(Long seekguIdx) {
+        return findSeekguBoardById(seekguIdx);
+    }
+}
