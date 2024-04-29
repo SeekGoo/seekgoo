fbb7c5acc7461a1dd1fcae7d59038246bd29bb61
chansoo <chansoo38@gmail.com>
2024-04-29 16:11:45 +0900

    refactor : SeekguBoard Create 메서드 로직 수정

--- seekgu/src/main/java/com/seekgu/seekguboard/service/SeekguBoardService.java
+++ seekgu/src/main/java/com/seekgu/seekguboard/service/SeekguBoardService.java
@@ -1,5 +1,7 @@
 package com.seekgu.seekguboard.service;
 
+import com.seekgu.participant.domain.Participant;
+import com.seekgu.participant.repository.ParticipantRepository;
 import com.seekgu.seekguboard.domain.SeekguBoard;
 import com.seekgu.seekguboard.repository.SeekguBoardRepository;
 import java.util.List;
@@ -12,14 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class SeekguBoardService {
 
     private final SeekguBoardRepository seekguBoardRepository;
+    private final ParticipantRepository participantRepository;
 
     @Transactional
     public Boolean createSeekguBoard(SeekguBoard seekguBoard) {
         try {
-            return seekguBoardRepository.save(seekguBoard);
+            Long saveIdx = seekguBoardRepository.save(seekguBoard);
+            Participant participant = Participant.builder()
+                    .memberIdx(seekguBoard.getMemberIdx())
+                    .seekguIdx(saveIdx)
+                    .build();
+            participantRepository.saveParticipant(participant);
         } catch (Exception e) {
             return Boolean.FALSE;
         }
+        return Boolean.TRUE;
     }
 
     public List<SeekguBoard> mySeekguBoards(Long memberIdx) {
