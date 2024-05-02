081611a98227ead7b117bbe62f90ea974e03d855
chansoo <chansoo38@gmail.com>
2024-05-02 17:16:22 +0900

    feat : SeekguBoardPreViewDto 추가

---
+++ seekgu/src/main/java/com/seekgu/seekguboard/domain/dto/SeekguBoardPreViewDto.java
@@ -0,0 +1,33 @@
+package com.seekgu.seekguboard.domain.dto;
+
+import com.seekgu.seekguboard.domain.SeekguBoard;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class SeekguBoardPreViewDto {
+    private String boardTitle;
+    private String boardRestaurantName;
+    private String boardMealTime;
+    private Integer boardMemberCount;
+    private Integer boardMax;
+    private LocalDateTime boardStartTime;
+    private Integer boardLimitTime;
+    private Boolean isRecruiting;
+
+    public SeekguBoardPreViewDto(SeekguBoard seekguBoard) {
+        this.boardTitle = seekguBoard.getSeekguTitle();
+        this.boardRestaurantName = seekguBoard.getSeekguRestaurantName();
+        this.boardMealTime = seekguBoard.getSeekguMealTime().name();
+        this.boardMemberCount = seekguBoard.getSeekguMemberCount();
+        this.boardMax = seekguBoard.getSeekguMax();
+        this.boardStartTime = seekguBoard.getSeekguRegDate();
+        this.boardLimitTime = seekguBoard.getSeekguLimitTime();
+        this.isRecruiting = checkRecruiting();
+    }
+
+    private Boolean checkRecruiting() {
+        return LocalDateTime.now().isBefore(boardStartTime.plusMinutes(boardLimitTime));
+    }
+
+}
