7d0f47f9c7a2d2809f0c4b2c5c4f3ec9eb8662b9
chansoo <chansoo38@gmail.com>
2024-05-07 15:45:38 +0900

    feat : 식구 모집글 관련 예외 정의

---
+++ seekgu/src/main/java/com/seekgu/seekguboard/exception/AlreadyParticipateException.java
@@ -0,0 +1,7 @@
+package com.seekgu.seekguboard.exception;
+
+public class AlreadyParticipateException extends RuntimeException {
+    public AlreadyParticipateException(String message) {
+        super(message);
+    }
+}
