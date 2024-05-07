a29efc7c6b9372c168db7c9e56b77b59c6d4821e
chansoo <chansoo38@gmail.com>
2024-05-07 16:02:51 +0900

    refactor : 카카오 예외 및 식구글 관련 예외 핸들링

--- seekgu/src/main/java/com/seekgu/config/exception/ExceptionController.java
+++ seekgu/src/main/java/com/seekgu/config/exception/ExceptionController.java
@@ -3,8 +3,12 @@ package com.seekgu.config.exception;
 import com.seekgu.member.exception.AlreadyExistsIdOrNickNameException;
 import com.seekgu.member.exception.NotExistUserException;
 import com.seekgu.member.exception.NotMatchPasswordException;
+import com.seekgu.seekguboard.exception.AlreadyDoneException;
+import com.seekgu.seekguboard.exception.AlreadyParticipateException;
+import com.seekgu.seekguboard.exception.SeekguBoardWriteException;
 import com.seekgu.utils.ApiUtil;
 import com.seekgu.utils.ApiUtil.ApiErrorResult;
+import com.seekgu.utils.kakao.exception.KaKaoInvalidParsingException;
 import com.seekgu.utils.slack.exception.SlackException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -35,13 +39,17 @@ public class ExceptionController {
         return ApiUtil.error(ErrorCode.DUPLICATE_KEY.getCode(), e.getMessage());
     }
 
-    @ExceptionHandler(SlackException.class)
-    public ApiErrorResult<String> handleSlackException(SlackException e) {
-        log.error(e.getMessage());
-        return ApiUtil.error(ErrorCode.UNKNOWN_ERROR.getCode(), "서버 동작과정에서 오류가 발생하였습니다.");
+    @ExceptionHandler(SeekguBoardWriteException.class)
+    public ApiErrorResult<String> handleSeekguBoardWriteException(SeekguBoardWriteException e) {
+        return ApiUtil.error(ErrorCode.BOARD_NOT_WRITE.getCode(), e.getMessage());
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler({AlreadyParticipateException.class, AlreadyDoneException.class})
+    public ApiErrorResult<String> handleParticipateException(RuntimeException e) {
+        return ApiUtil.error(ErrorCode.CANT_PARTICIPATE.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class, SlackException.class, KaKaoInvalidParsingException.class})
     public ApiErrorResult<String> handleRuntimeException(RuntimeException e) {
         log.error(e.getMessage());
         return ApiUtil.error(ErrorCode.UNKNOWN_ERROR.getCode(), "서버 동작과정에서 오류가 발생하였습니다.");
