bdc13ae9ce7f6005e264094bb8b3debfa0eef451
jiyeon <sjy6736@naver.com>
2024-05-08 14:26:17 +0900

    feat: 리뷰관련 에러 코드 추가

---
+++ seekgu/src/main/java/com/seekgu/review/exception/DataLengthTooLongException.java
@@ -0,0 +1,7 @@
+package com.seekgu.review.exception;
+
+public class DataLengthTooLongException extends RuntimeException {
+    public DataLengthTooLongException(String message) {
+        super(message);
+    }
+}
